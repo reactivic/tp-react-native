4ae03c9fd7e2bc33fa66cf9a470f856163d14460
sabativi <victor.sabatier@gmail.com>
2018-03-06 17:11:50 +0100

    add list with react-native-elements

--- ListScreen.js
+++ ListScreen.js
@@ -1,16 +1,53 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import { ListItem } from "react-native-elements";
+
+const JobIsTaken = () => (
+  <Ionicons
+    size={30}
+    name="ios-checkmark-circle"
+    color="green"
+  />
+);
+
+const _keyExtractor = item => item.id;
 
 export default class ListScreen extends React.Component {
   static navigationOptions = {
     tabBarLabel: "Liste",
     tabBarIcon: ({ tintColor }) => <Ionicons name="ios-list" size={25} color={tintColor} />,
   };
+  constructor(props) {
+    super(props);
+    this.state = {
+      jobs: [],
+    }
+  }
+  componentDidMount() {
+    fetch('https://mobile-api-jobs.herokuapp.com/api/jobs')
+    .then(data => data.json())
+    .then(jobs => {
+      this.setState({ jobs })
+    })
+    .catch(console.error)
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>La liste ici</Text>
+        <FlatList
+          style={styles.container}
+          data={this.state.jobs}
+          keyExtractor={_keyExtractor}
+          renderItem={({item}) =>
+            <ListItem
+              key={item.id}
+              title={item.name}
+              hideChevron
+              badge={item.status === "true" ? { element: <JobIsTaken /> } : { element: <View /> }}
+            />
+          }
+        />
       </View>
     );
   }
@@ -20,7 +57,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
