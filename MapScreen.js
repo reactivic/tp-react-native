c3914a6d43c03a228825c20fb2a83ec98ac860ec
sabativi <victor.sabatier@gmail.com>
2018-03-06 15:38:35 +0100

    add map with markers

--- MapScreen.js
+++ MapScreen.js
@@ -1,17 +1,50 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import { MapView } from 'expo';
 
 export default class MapScreen extends React.Component {
   static navigationOptions = {
     tabBarLabel: "Carte",
     tabBarIcon: ({ tintColor }) => <Ionicons name="ios-map" size={25} color={tintColor} />,
   };
+  constructor(props) {
+    super(props);
+    this.state = {
+      jobs: [],
+    }
+  }
+  componentDidMount() {
+    fetch('https://mobile-api-jobs.herokuapp.com/api/jobs')
+    .then(data => data.json())
+    .then(jobs => {
+      this.setState({ jobs })
+    })
+    .catch(console.error)
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Notre carte</Text>
-      </View>
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 100,
+          longitudeDelta: 100,
+        }}
+      >
+        {this.state.jobs.map(job => (
+          <MapView.Marker
+            key={job.id}
+            coordinate={{
+              latitude: parseInt(job.latitude),
+              longitude: parseInt(job.longitude),
+            }}
+            pinColor={job.status === "true" ? 'green' : 'red'}
+            title={job.name}
+          />
+        ))}
+      </MapView>
     );
   }
 }
